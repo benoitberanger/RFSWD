6250f4e9de5bc7f12b5673fb18a90b5a9c42992b
Benoît Béranger <benoit.beranger@icm-institute.org>
2025-02-04 15:33:39 +0100

    parse all .csv files from the CWD, log ok

--- parse_csv_RFSWD.py
+++ parse_csv_RFSWD.py
@@ -1,15 +1,22 @@
+# builtin modules
+import os
+import pathlib
 import csv
 
+cwd = os.getcwd()
 
-filepath = 'RFinfo_20250129_human_subject/RFSWDHistoryListNew.csv'
+# fetch all .csv files
+all_files = sorted(pathlib.Path(cwd).rglob('*csv'))
+if len(all_files)==0:
+    raise RuntimeError(f'No .csv file recurivelty from this dir : {cwd}')
 
+# read all files & store data
 seq = []
-with open(file=filepath, mode='r') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=';')
-    for seq_dict in reader:
-        seq.append(seq_dict)
-
-n = len(seq)
+for filepath in all_files:
+    with open(file=filepath, mode='r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        for seq_dict in reader:
+            seq.append(seq_dict)
 
 # rowname = seq[0].keys()
 # with open('list_columns.txt', mode='w') as fid:
@@ -17,6 +24,7 @@ n = len(seq)
 #         # print(f"{row_idx} {row_name}")
 #         fid.writelines(f"{row_idx} {row_name} \n")
 
+n = len(seq)
 len_SeqName :int = 0
 len_ProtName:int = 0
 for idx in range(n):
@@ -36,8 +44,10 @@ for idx in range(n):
         HeadLocal_relative_WperKg = HeadLocal_value_WperKg / LIM_HeadLocal
 
         line = (
+            f"{seq[idx]['PatID']} {seq[idx]['Mass']:2s} {seq[idx]['Age']:3s} {seq[idx]['Size']:4s} {seq[idx]['Sex']} " 
             f"{seq[idx]['Date']} {seq[idx]['Time']} {seq[idx]['SeqName']:{len_SeqName}s} {seq[idx]['ProtName']:{len_ProtName}s} - "
-            f"PREDICTED (W/Kg ~ relative): Head {Head_value_WperKg: 7.3f} ~ {Head_relative_WperKg: 7.3f} HeadLocal {HeadLocal_value_WperKg: 7.3f} ~ {HeadLocal_relative_WperKg: 7.3f}"
+            f"PREDICTED (W/Kg ~ relative): Head ({Head_value_WperKg: 7.3f} ~ {Head_relative_WperKg: 7.3f}) HeadLocal ({HeadLocal_value_WperKg: 7.3f} ~ {HeadLocal_relative_WperKg: 7.3f})"
                )
         
         print(line)
+        
