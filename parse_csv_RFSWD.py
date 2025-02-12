15e762ad9a023f6c34317f24dd4386352672060b
Benoît Béranger <benoit.beranger@icm-institute.org>
2025-02-12 19:43:02 +0100

    constant values more clean

--- parse_csv_RFSWD.py
+++ parse_csv_RFSWD.py
@@ -6,7 +6,9 @@ import pathlib
 import csv
 
 # config
-MARSDIR: str = '/opt/medcom/log/'
+MARSDIR      : str   = '/opt/medcom/log/'
+LIM_HEAD     : float =  3.2
+LIM_HEADLOCAL: float = 20.0
 
 if os.path.exists(MARSDIR):
     target_dir = MARSDIR
@@ -40,9 +42,6 @@ for idx in range(n):
     len_SeqName  = max(len(seq[idx][ 'SeqName']),len_SeqName)
     len_ProtName = max(len(seq[idx]['ProtName']),len_ProtName)
 
-LIM_Head     : float =  3.2
-LIM_HeadLocal: float = 20.0
-
 for idx in range(n):
     if '%AdjustSeq%'  in seq[idx]['SeqName']: continue
     if '%ServiceSeq%' in seq[idx]['SeqName']: continue
@@ -51,13 +50,13 @@ for idx in range(n):
         Head_value_WperKg = float(seq[idx]['AspVal[2][0]'])
     else:
         Head_value_WperKg = -2
-    Head_relative_WperKg = Head_value_WperKg / LIM_Head
+    Head_relative_WperKg = Head_value_WperKg / LIM_HEAD
 
     if len(seq[idx]['AspVal[3][0]'])>0:
         HeadLocal_value_WperKg = float(seq[idx]['AspVal[3][0]'])
     else:
         HeadLocal_value_WperKg = -2
-    HeadLocal_relative_WperKg = HeadLocal_value_WperKg / LIM_HeadLocal
+    HeadLocal_relative_WperKg = HeadLocal_value_WperKg / LIM_HEADLOCAL
 
     line = (
         f"{seq[idx]['Mass']:2s} {seq[idx]['Age']:3s} {seq[idx]['Size']:4s} {seq[idx]['Sex']} " 
